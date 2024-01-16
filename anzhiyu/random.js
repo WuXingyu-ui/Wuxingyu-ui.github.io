b1fe61fff15e1df5790c779c470962f8322e79ba
小吴同学 <wuxingyu0125@outlook.com>
2024-01-16 12:09:05 +0800

    Site updated: 2024-01-16 12:09:02

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/01/15/第一篇文章/"];function toRandomPost(){
+var posts=["2024/01/15/高中生/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
