6e0495436aa7edcdadae2d879c0e17b1150f7287
小吴同学 <wuxingyu0125@outlook.com>
2024-01-16 11:51:06 +0800

    Site updated: 2024-01-16 11:51:05

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/01/15/高中生/"];function toRandomPost(){
+var posts=["2024/01/16/高中生/","2024/01/16/高中生活/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
